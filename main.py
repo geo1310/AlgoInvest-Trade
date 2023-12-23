aadeb8fa751c4ca459a236253e16a55b20a7d96e
Geo1310 <gbriche59@yahoo.fr>
2023-12-23 17:51:49 +0100

    optimisation du code

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from itertools import combinations
 
 
+# variables
+actions_list = [
+
+    ('Action-1', 20, 1),
+    ('Action-2', 30, 3),
+    ('Action-3', 50, 7.5),
+    ('Action-4', 70, 14),
+    ('Action-5', 60, 10.2),
+    ('Action-6', 80, 20),
+    ('Action-7', 22, 1.54),
+    ('Action-8', 26, 2.86),
+    ('Action-9', 48, 6.24),
+    ('Action-10', 34, 9.18),
+    ('Action-11', 42, 7.14),
+    ('Action-12', 110, 9.9),
+    ('Action-13', 38, 8.74),
+    ('Action-14', 14, 0.14),
+    ('Action-15', 18, 0.54),
+    ('Action-16', 8, 0.64),
+    ('Action-17', 4, 0.48),
+    ('Action-18', 10, 1.4),
+    ('Action-19', 24, 5.04),
+    ('Action-20', 114, 20.52)
+]
+
+amount = 500
+
+
 def recursive_brute_force(amount: int, actions_list: list, actions_selection: list = None):
     """
     Algorithme de force brute avec récursivité : Calcule toutes les possibilités
@@ -47,47 +75,16 @@ def itertools_brute_force(amount: int, actions_list: list):
     return profit_total_final, actions_selection_final
 
 
-# variables
-actions_list = [
+def result_display(profit_total, actions_selection):
+    print("\nListe d'actions: \n")
+    for action in actions_selection:
+        name, price, profit = action
+        print(f"Nom : {name}  \tPrix : {price}  \tProfit : {profit} ")
+    print(f"\nProfit total : {profit_total} \tCout Total : {sum([i[1] for i in actions_selection])}\n")
 
-    ('Action-1', 20, 1),
-    ('Action-2', 30, 3),
-    ('Action-3', 50, 7.5),
-    ('Action-4', 70, 14),
-    ('Action-5', 60, 10.2),
-    ('Action-6', 80, 20),
-    ('Action-7', 22, 1.54),
-    ('Action-8', 26, 2.86),
-    ('Action-9', 48, 6.24),
-    ('Action-10', 34, 9.18),
-    ('Action-11', 42, 7.14),
-    ('Action-12', 110, 9.9),
-    ('Action-13', 38, 8.74),
-    ('Action-14', 14, 0.14),
-    ('Action-15', 18, 0.54),
-    ('Action-16', 8, 0.64),
-    ('Action-17', 4, 0.48),
-    ('Action-18', 10, 1.4),
-    ('Action-19', 24, 5.04),
-    ('Action-20', 114, 20.52)
-]
-
-amount = 500
 
 # méthode récursive
-profit_total, actions_selection = recursive_brute_force(amount, actions_list)
-
-print("\nListe d'actions: \n")
-for action in actions_selection:
-    name, price, profit = action
-    print(f"Nom : {name}  \tPrix : {price}  \tProfit : {profit} ")
-print(f"\nProfit total : {profit_total} \tCout Total : {sum([i[1] for i in actions_selection])}\n")
+result_display(*recursive_brute_force(amount, actions_list))
 
 # méthode itertools.combinations
-profit_total, actions_selection = itertools_brute_force(amount, actions_list)
-
-print("\nListe d'actions: \n")
-for action in actions_selection:
-    name, price, profit = action
-    print(f"Nom : {name}  \tPrix : {price}  \tProfit : {profit} ")
-print(f"\nProfit total : {profit_total} \tCout Total : {sum([i[1] for i in actions_selection])}\n")
+result_display(*itertools_brute_force(amount, actions_list))
