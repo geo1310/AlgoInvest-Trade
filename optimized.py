85358ae88da3e2a52e665eec9eb3aafbb19ee68c
Georges Briche <gbriche59@yahoo.fr>
2024-01-12 13:25:11 +0100

    version sans modules

--- optimized.py
+++ optimized.py
@@ -1,7 +1,6 @@
 import pandas as pd
-
-from modules.display import result_display
-from modules.performances import execution_time
+import timeit
+import psutil
 
 
 # variables
@@ -78,6 +77,51 @@ def dynamic_method(amount: float, actions_list: list, type: int):
     ]
 
     return matrice[-1][-1] / 1, actions_list_selection
+
+
+# affichage des resultats
+def result_display(results: tuple):
+    profit_total = results[0][0]
+    actions_selection = results[0][1]
+    execution_time = results[1]
+    memory_used = results[2]
+    cpu_percent = results[3]
+
+    print("\nListe d'actions: \n")
+    index = 1
+    for action in actions_selection:
+        name, price, profit = action
+        print(f"{index}\tNom : {name}  \tPrix : {price}  \tProfit : {profit} ")
+        index += 1
+    total_cost = round(sum([action[1] for action in actions_selection]), 2)
+    print(
+        f"\nProfit total : {round(profit_total, 2)} \tCout Total : {total_cost}\n"
+    )
+    print(f"Temps d'execution : {execution_time} ms.")
+    print(f"Utilisation mémoire : {memory_used} bytes.")
+    print(f"Utilisation CPU : {cpu_percent} %.\n")
+
+
+# calcul du temps d execution et les ressources mémoire et CPU d'une fonction
+def execution_time(function):
+    # Mesurer la mémoire avant l'exécution
+    start_memory = psutil.Process().memory_info().rss
+
+    temps_debut = timeit.default_timer()
+    resultat = function()
+    temps_fin = timeit.default_timer()
+
+    # Mesurer la mémoire après l'exécution
+    end_memory = psutil.Process().memory_info().rss
+    memory_used = end_memory - start_memory
+
+    # durée d execution en millisecondes
+    time_execution = round(((temps_fin - temps_debut) * 1000), 2)
+
+    # Mesurer l'utilisation du CPU
+    cpu_percent = psutil.cpu_percent(interval=1)  # Utilisation CPU au cours de la dernière seconde
+
+    return resultat, time_execution, memory_used, cpu_percent
 
 
 # méthode dynamique
